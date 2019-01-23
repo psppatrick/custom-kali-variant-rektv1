2872dc1551b196aafe89a2f4369a54d7cdbebf43
psppatrick <119426@student.eastkent.ac.uk>
2019-01-23 11:45:15 +0000

    Initial File Upload
    Added live-build common files and variant directory for rektv1

---
+++ common/includes.chroot/root/InstallLoic.bash
@@ -0,0 +1,25 @@
+﻿#!/bin/bash
+
+mkdir loic
+
+cd loic
+
+sleep 0.05
+
+wget https://raw.github.com/nicolargo/loicinstaller/master/loic.sh
+
+sleep 1s
+
+chmod a+x loic.sh
+
+sleep 0.05
+
+./loic.sh install
+
+sleep 0.05
+
+./loic.sh update
+
+sleep 0.05
+
+# ./loic.sh run
---
+++ common/includes.chroot/root/UpdateMe.bash
@@ -0,0 +1,21 @@
+﻿#!/bin/bash
+
+apt update
+
+sleep 1s
+
+apt upgrade -y
+
+echo "Finished Updating! Will you run autoremove?(y/n)"
+
+read $response
+
+if $response "Y"
+then
+	apt autoremove
+elif $response "y"
+then
+	apt autoremove
+else
+	echo "Not running autoremove, please consider running later to clean wasted space"
+fi
