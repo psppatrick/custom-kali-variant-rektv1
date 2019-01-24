cf83cc0546da0b1e56c0241d8a25d814548220e6
psppatrick <119426@student.eastkent.ac.uk>
2019-01-24 12:27:26 +0000

    ARM64 scripts
    Initial Commit to include ARM64 scripts for Cross-Compilation and setting up Kali ARM chroot
    
    Sources:
    https://docs.kali.org/development/arm-cross-compilation-environment
    https://docs.kali.org/development/kali-linux-arm-chroot

---
+++ ARMBuildPrep.bash
@@ -0,0 +1,10 @@
+﻿#!/bin/bash
+
+cd ~
+
+git clone https://github.com/offensive-security/kali-arm-build-scripts.git
+
+apt update
+
+apt -y install debootstrap qemu-user-static device-tree-compiler lzma lzop u-boot-tools libncurses5:i386 pixz
+
---
+++ ARMCrossCompilation.bash
@@ -0,0 +1,30 @@
+﻿#!/bin/bash
+
+# This script will install all dependencies for ARM cross-compilation
+
+apt install git-core gnupg flex bison gperf libesd0-dev build-essential \
+
+zip curl libncurses5-dev zlib1g-dev gcc-multilib g++-multilib
+
+dpkg --add-architecture i386
+
+apt update
+
+apt install ia32-libs apt-cacher-ng
+
+sleep 1s
+
+cd ~
+
+mkdir -p arm-stuff/kernal/toolchains
+
+cd arm-stuff/kernal/toolchains
+
+git clone git://github.com/offensive-security/gcc-arm-eabi-linaro-4.6.2.git
+
+sleep 5s
+
+export ARCH=arm
+#export CROSS_COMPILE=~/arm-stuff/kernel/toolchains/gcc-arm-eabi-linaro-4.6.2/bin/arm-eabi-
+
+echo "Done!"
