4d56da57bdc1bfb1c36b0472d1451cfb2501c67c
Patrick Hussey <patrick.hussey@me.com>
2019-01-31 12:08:31 +0000

    Updates
    I'm getting tired of this build not working properly

--- X64RektV1.sh
+++ X64RektV1.sh
@@ -20,17 +20,24 @@ git clone git://git.kali.org/live-build-config.git
 
 git clone https://github.com/psppatrick/custom-kali-variant-rektv1.git
 
-cp ~/ISOBuilding/custom-kali-variant-rektv1/kali.list.chroot live-build-config/kali-config/variant-light/package-lists/kali.list.chroot; echo $?
+mkdir ~/ISOBuilding/live-build-config/kali-config/variant-custom
 
-cp ~/ISOBuilding/custom-kali-variant-rektv1/InstallLOIC.sh live-build-config/kali-config/common/includes.chroot/InstallLOIC.sh
+mkdir ~/ISOBuilding/live-build-config/kali-config/variant-custom/package-lists
 
-cp ~/ISOBuilding/custom-kali-variant-rektv1/UpdateMe.sh live-build-config/kali-config/common/includes.chroot/InstallLOIC.sh
+cp ~/ISOBuilding/custom-kali-variant-rektv1/kali.list.chroot ~/ISOBuilding/live-build-config/kali-config/variant-custom/package-lists/kali.list.chroot; echo $?
+
+cp ~/ISOBuilding/custom-kali-variant-rektv1/InstallLOIC.sh ~/ISOBuilding/live-build-config/kali-config/common/includes.chroot/InstallLOIC.sh
+
+cp ~/ISOBuilding/custom-kali-variant-rektv1/UpdateMe.sh ~/ISOBuilding/live-build-config/kali-config/common/includes.chroot/UpdateMe.sh
 
 cd ~/ISOBuilding/live-build-config/
 
-./build.sh --variant light --verbose; echo $?; finishTime=$(date)
+./build.sh --variant custom --verbose -- \ --apt-http-proxy=${http+proxy}; echo $?; finishTime=$(date)
 
 sleep 0.05
 
 echo "Start time was" $startTime "Finish time was" $finishTime >> ~/x64BuildLog.txt
 
+echo "Moving File to the exit directory"
+
+cp 
