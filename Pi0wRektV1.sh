a234090974af2869a9f13b70a31d3242672d29db
Patrick Hussey <patrick.hussey@me.com>
2019-02-04 11:24:48 +0000

    Ffs

--- Pi0wRektV1.sh
+++ Pi0wRektV1.sh
@@ -43,6 +43,6 @@ cp ~/arm-stuff/custom-kali-variant-rektv1/rpi0w-nexmon-Rektv1.sh ~/arm-stuff/kal
 
 sleep 0.05
 
-/arm-stuff/kali-arm-build-scripts/rpi0w-nexmon-Rektv1.sh 1.0; echo $?; finishTime=$(date); echo "Time started" $startTime "Time Finished" $finishTime >> ~/Pi0wbuildlog.txt
+~/arm-stuff/kali-arm-build-scripts/rpi0w-nexmon-Rektv1.sh 1.0; echo $?; finishTime=$(date); echo "Time started" $startTime "Time Finished" $finishTime >> ~/Pi0wbuildlog.txt
 
 echo "Finished!"
